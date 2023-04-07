c81d07284dee27b9e2d96d244ed45e590b2a5a59
naitea9527 <a22057916w@gmail.com>
2023-04-08 01:06:10 +0800

    rm private section and attr

--- gateOR.h
+++ gateOR.h
@@ -2,14 +2,12 @@
 #inclide "Device.h"
 
 class gateOR : public Device {
-private:
-  int inputVal = 0;   // this value might not be inputed
 public:
-  gateOR(int iVal = 0): inputVal(iVal) {};
+  gateOR() {};
 
   int getOutput() const override {
-    int output = this->inputVal;
-    for auto dev in iPins:
+    int output = 0;
+    for(auto dev : iPins)
       output |= dev.getOutput();
     return output;
   }
