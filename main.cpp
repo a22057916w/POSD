78fd4d94276037e20f842ba31953bc8d79a60543
naitea9527 <a22057916w@gmail.com>
2023-04-07 19:50:45 +0800

    include logicsimulator.h, textui.h

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
+#include "logicsimulator.h"
+#include "textui.h"
 #include <iostream>
 
 int main() {
+  LogicSimulator *LS = new LogicSimulator();
+  TextUI textUI(LS);
+  textUI.displayMenu();
+  textUI.processCommand();
   return 0;
 }
