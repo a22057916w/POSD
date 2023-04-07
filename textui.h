e3e9fdd81e5051d7d9f98035e3bf823b4691cfb9
naitea9527 <a22057916w@gmail.com>
2023-04-07 22:25:42 +0800

    ch switch to if-else

--- textui.h
+++ textui.h
@@ -15,38 +15,36 @@ public:
       << "2. Simulation\n"
       << "3. Display truth table\n"
       << "4. Exit\n";
+    cout << "Command:";
   }
   void processCommand() {
     int command = -1;
     while(cin >> command && command != 4) {
-      string filename;
+      if(command == 1) {
+        string filename;  // path of lcf file
 
-      switch(command) {
-        case 1:
-          cout << "Command:1" << endl;
+        cout << "Please key in a file path: ";
+        cin >> filename;
 
-          cout << "Please key in a file path:" << endl;
-          cin >> filename;
-
-          if(LS_ptr->load(filename))
-            cout << "tmp" << endl;
-          else
-            cout << "File not found or file format error!!" << endl;
-          break;
-        case 2:
-          cout << "Command:2" << endl;
-          break;
-        case 3:
-          cout << "Command:3" << endl;
-          break;
-        case 4:
-          cout << "Command:4" << endl;
-          break;
-        default:
-          cout << "Invalid command." << endl;
-          break;
+        if(LS_ptr->load(filename))
+          cout << "tmp" << endl;
+        else
+          cout << "File not found or file format error!!" << endl;
       }
-  }
+      else if(command == 2) {
+        cout << command << endl;
+      }
+      else if(command == 3) {
+        cout << command << endl;
+      }
+      else if(command == 4) {
+        cout << "Goodbye, thanks for using LS." << endl;
+        break;
+      }
+      else
+          cout << "Invalid command." << endl;
 
+      displayMenu();
+    }
   }
 };
