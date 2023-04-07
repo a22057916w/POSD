fece110c104f7120ecdcda072554360e6ddab3b4
naitea9527 <a22057916w@gmail.com>
2023-04-08 00:00:31 +0800

    Create gateNOT.h

---
+++ gateNOT.h
@@ -0,0 +1,16 @@
+#pragma once
+#inclide "Device.h"
+
+class gateAND : public Device {
+private:
+  int inputVal = 0;   // this value might not be inputed
+public:
+  gateAND(int iVal = 0): inputVal(iVal) {};
+
+  int getOutput() const override {
+    int output = this->inputVal;
+    for auto dev in iPins:  // the size of iPins should be one
+      output -= dev.getOutput();
+    return output;
+  }
+};
