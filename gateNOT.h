de635beb12cf817a06d28a9c122a0e050733c247
naitea9527 <a22057916w@gmail.com>
2023-04-08 20:54:41 +0800

    rm constructor and fix syntax

--- gateNOT.h
+++ gateNOT.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "Device.h"
 
-class gateAND : public Device {
-private:
-  int inputVal = 0;   // this value might not be inputed
+class gateNOT : public Device {
 public:
-  gateAND(int iVal = 0): inputVal(iVal) {};
+  gateNOT() {};
 
   int getOutput() const override {
-    int output = this->inputVal;
-    for auto dev in iPins:  // the size of iPins should be one
-      output -= dev.getOutput();
+    int output = 0;
+    for(auto dev : iPins)  // the size of iPins should be one
+      output -= dev->getOutput();
     return output;
+  }
+
+  string getType() const override {
+    return "gateNOT";
   }
 };
