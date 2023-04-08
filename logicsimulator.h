d6299344d2ba16561c74e9b868a37136ac47fe35
naitea9527 <a22057916w@gmail.com>
2023-04-08 23:48:02 +0800

    seperate imp code

--- logicsimulator.h
+++ logicsimulator.h
@@ -1,16 +1,28 @@
-#pargma once
-#include "device.h"
+#pragma once
+
+#include "Device.h"
+#include "gateAND.h"
+#include "gateNOT.h"
+#include "gateOR.h"
+#include "iPin.h"
+#include "oPin.h"
+
 #include <vector>
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+
 using namespace std;
 
 class LogicSimulator {
 private:
-  vector<device *> circuit;
-  vector<device *> iPins;
-  vector<device *> oPins;
+  vector<Device *> circuit;
+  vector<Device *> iPins;
+  vector<oPin *> oPins;
 public:
   string getSimulationResult(vector<string> v_tmp);
   string getTruthTable();
-  bool load(string s);
-}
+  bool load(string filename);
+  bool isLoaded();
+};
