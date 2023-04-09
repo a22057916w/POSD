d7ef5d591b00869460d2e99458af7bb31d8dea59
naitea9527 <a22057916w@gmail.com>
2023-04-09 12:03:30 +0800

    rm params of getSimulationResulot

--- logicsimulator.h
+++ logicsimulator.h
@@ -23,7 +23,7 @@ private:
 
   bool is_loaded = false;
 public:
-  string getSimulationResult(vector<string> v_tmp);
+  string getSimulationResult();
   string getTruthTable();
   string getLayout();
 
